df15949d50f04bb2463cbb70d3b970aa74b939c4
RicardoAli <61981276+RicardoAli@users.noreply.github.com>
2025-09-23 02:17:03 +0000

    Final Version

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ import "bootstrap";
 import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+//import "./assets/img/rigo-baby.jpg";
+//import "./assets/img/4geeks.ico";
 
 window.onload = function() {
- 
-  let paragraph = document.querySelector("excuse")
-  paragraph.innerText = "This is my first excuse"
 
-  console.log("Hello Rigo from the console!");
+  let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+  let action = ['ate', 'peed', 'crushed', 'broke'];
+  let what = ['my homework', 'my phone', 'the car'];
+  let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+
+  let excusePart1 = who[Math.floor(Math.random() * who.length)];
+  let excusePart2 = action[Math.floor(Math.random() * action.length)];
+  let excusePart3 = what[Math.floor(Math.random() * what.length)];
+  let excusePart4 = when[Math.floor(Math.random() * when.length)];
+
+  let excuse = excusePart1 + " " + excusePart2 + " " + excusePart3 + " " + excusePart4;
+
+  console.log(excuse);
+
+  let element = document.querySelector("#excuse")
+  element.innerText = excuse;
+
+  //console.log("Hello Rigo from the console!");
 };
